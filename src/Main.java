952a75678d75b153a729207c00ec798806dbc409
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-20 09:52:04 +0100

    Intial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Givet klassen RatNum som representerar ett rationellt tal, lägg till två metoder:
+        //   - mul --> Multiplicera med ett annat rationellt tal
+        //   - add --> Addera ett annat rationellt tal
+
+    }
+}
+
+class RatNum {
+    // p / q
+    int p;
+    int q;
+}
