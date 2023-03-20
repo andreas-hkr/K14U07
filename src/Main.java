7f61ef17adbadedf70aaa0dca8b12d1b12c464a4
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-20 10:05:04 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,17 @@ public class Main {
         //   - mul --> Multiplicera med ett annat rationellt tal
         //   - add --> Addera ett annat rationellt tal
 
+        RatNum r1 = new RatNum();
+        r1.p = 2;
+        r1.q = 3;
+
+        RatNum r2 = new RatNum();
+        r2.p = 2;
+        r2.q = 7;
+
+        r1.add(r2);
+
+        System.out.format("%d/%d", r1.p, r1.q);
     }
 }
 
@@ -11,4 +22,14 @@ class RatNum {
     // p / q
     int p;
     int q;
+
+    void mul(RatNum r) {
+        p = p * r.p;
+        q = q * r.q;
+    }
+
+    void add(RatNum r) {
+        p = p * r.q + r.p * q;
+        q = q * r.q;
+    }
 }
